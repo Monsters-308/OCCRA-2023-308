639992f60737806c91a58f9284914ed3ed3c36ab
NaN0987 <116316543+NaN0987@users.noreply.github.com>
2023-09-19 19:50:47 -0400

    added mecanum code

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
+
+# Documentation link: https://www.robotmesh.com/studio/content/docs/vexv5-python/html/annotated.html
+
 # ---------------------------------------------------------------------------- #
 #                                                                              #
 # 	Module:       main.py                                                      #
-# 	Author:       noahn                                                        #
+# 	Author:       Noah Nicolas Gabe Jerry                                      #
 # 	Created:      9/13/2023, 1:06:06 PM                                        #
 # 	Description:  V5 project                                                   #
 #                                                                              #
@@ -9,6 +12,7 @@
 
 # Library imports
 from vex import *
+import math
 
 # Brain should be defined by default
 brain=Brain()
@@ -22,58 +26,125 @@ brain=Brain()
 
 # Configure devices
 frontLeftMotor = Motor29(brain.three_wire_port.a, False)
-FrontRightMotor = Motor29(brain.three_wire_port.b, False)
+frontRightMotor = Motor29(brain.three_wire_port.b, False)
 backLeftMotor = Motor29(brain.three_wire_port.d, True)
 backRightMotor = Motor29(brain.three_wire_port.e, True)
+flywheelMotor = Motor29(brain.three_wire_port.f, False)
 controller_1 = Controller(PRIMARY)
+controller_2 = Controller(PARTNER)
 
 # wait for stuff to configure
 wait(25, MSEC)
 
 # Constants
 FORWARD_SPEED_MULTIPLIER = 1
+STRAFE_SPEED_MULTIPLIER = 1
 ROTATION_SPEED_MULTIPLIER = 1
 
 # Code ran once on startup
 def robotInit():
     pass 
 
+def hypot(x: float, y: float) -> float:
+    return math.sqrt(x**2+y**2)
 
 # Main programming loop
 def main():
-    # get joystick axises
-    Axis3 = controller_1.axis3.position() * FORWARD_SPEED_MULTIPLIER
-    Axis1 = controller_1.axis1.position() * -ROTATION_SPEED_MULTIPLIER
+    x = controller_1.axis3.position() * FORWARD_SPEED_MULTIPLIER
+    y = controller_1.axis4.position() * STRAFE_SPEED_MULTIPLIER
+    turn = controller_1.axis1.position() * ROTATION_SPEED_MULTIPLIER
+    
+    if abs(x) < 5:
+       x = 0
+    if abs(y) < 5:
+       y = 0
+    if abs(turn) < 5:
+       turn = 0
+
+    
+    theta = math.atan2(y,x)
+    power = hypot(x,y)
+        
+    brain.screen.clear_screen()
+    brain.screen.set_cursor(1,1)
     
 
-    # deadband for joystick drift
-    if abs(Axis3) < 5:
-        Axis3 = 0
-    if abs(Axis1) < 5:
-        Axis1 = 0
+
+    #deadband for joystick drift
+    
 
     # move drive wheels
-    drive(Axis3, Axis1)
+    drive(power, turn, theta+math.pi/2)
 
 
 # Moves the chassis wheels. forwardSpeed represents how fast forward, turningSpeed represents how fast you're turning (positive is right). 
 # Both are values from -100 to 100.
-def drive(forwardSpeed: float, turningSpeed: float):
+def drive(power: float, turn: float, theta: float):
+    sin = math.sin(theta - math.pi/4)
+    cos = math.cos(theta - math.pi/4)
+    maxValue = max(abs(sin), abs(cos))
     
-    # alters the speed of each side depending on which direction you're turning and by how much
-    leftSpeed = forwardSpeed + turningSpeed
-    rightSpeed = forwardSpeed - turningSpeed
+    leftFront = power * cos/maxValue + turn
+    rightFront = power * sin/maxValue - turn
+    leftRear = power * sin/maxValue + turn
+    rightRear = power * cos/maxValue - turn
+
+    # if one motor needs to move faster than the max, all of the motor speeds are reduced
+    if ((power + abs(turn)) > 100):
+        leftFront /= power + abs(turn) 
+        rightFront /= power + abs(turn) 
+        leftRear /= power + abs(turn)
+        rightRear /= power + abs(turn) 
+        leftFront *= 100
+        rightFront *= 100
+        leftRear *= 100
+        rightRear *= 100
     
+
+
+
+
+    
+
+
     # drive left side
-    frontLeftMotor.spin(FORWARD, leftSpeed)
-    backLeftMotor.spin(FORWARD, leftSpeed)
+    frontLeftMotor.spin(FORWARD, leftFront)
+    backLeftMotor.spin(FORWARD, leftRear)
 
     # drive right side
-    FrontRightMotor.spin(FORWARD, rightSpeed)
-    backRightMotor.spin(FORWARD, rightSpeed)
+    frontRightMotor.spin(FORWARD, rightFront)
+    backRightMotor.spin(FORWARD, rightRear)
+
+    # brain.screen.clear_screen()
+    # brain.screen.set_cursor(1,1)
+    # brain.screen.print("LF: ", leftFront)
+    # brain.screen.new_line()
+    # brain.screen.print("LR: ", leftRear)
+    # brain.screen.new_line()
+    # brain.screen.print("RF: ", rightFront)
+    # brain.screen.new_line()
+    # brain.screen.print("RR: ", rightRear)
+    # brain.screen.new_line()
+    # brain.screen.print("Theta ", theta*57.29)
+    # brain.screen.new_line()
+    # brain.screen.print("Power ", power)
+    # brain.screen.new_line()
+    # brain.screen.print("Turn ", turn)
+
+
+def intake(): 
+    pass 
+
+def shoot():
+    # only want to intake if button "a" is being pressed 
+    if controller_1.buttonA.pressing():     
+        flywheelMotor.spin(FORWARD, 50)
+    else:
+        flywheelMotor.stop()
 
 
 # ---- START ACTUALLY EXECUTING CODE ---- 
+
 robotInit()
 
 while 1:
