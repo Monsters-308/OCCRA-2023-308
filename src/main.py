414eba2db2c9d83636310a47c3357113aedbc54b
coloradoc <girzzzzwald19@gmail.com>
2023-10-07 23:41:21 -0400

    basic layout of how our robot will function
    This code can drive the robot, and run the intake and shooting motors. Needs to be polished, but the robot can intake balls from the human player station and shoot. Robot can also intake from ground, but it still needs an actual intake. Mecanum drivetrain is still broken, it is very important that we fix that. Slowing down front motors to match gear ratio did not fix it.

--- src/main.py
+++ src/main.py
@@ -18,11 +18,7 @@ import math
 # frontLeftMotor      motor29       A
 # frontRightMotor     motor29       B
 # backLeftMotor       motor29       D
-# backRightMotor      motor29       C
-#
-# redFlywheelMotor    motor29       ?
-# blueFlywheelMotor   motor29       ?
-# intakeWheelMotor    motor29       ?
+# backRightMotor      motor29       E
 # Bottom of Vexcode Configures Devices KEY---------
 
 
@@ -35,9 +31,11 @@ frontRightMotor = Motor29(brain.three_wire_port.b, True)
 backLeftMotor = Motor29(brain.three_wire_port.d, False)
 backRightMotor = Motor29(brain.three_wire_port.c, True)
 
-redFlywheelMotor = Motor29(brain.three_wire_port.f, False)
-blueFlywheelMotor = Motor29(brain.three_wire_port.g, True)
-intakeWheelMotor = Motor29(brain.three_wire_port.h, True)
+triport = Triport(Ports.PORT1)
+flywheelMotor1 = Motor29(triport.d, True)
+flywheelMotor2 = Motor29(triport.f, False)
+inakeMotor = Motor29(triport.a, False)
+conveyorMotor = Motor29(triport.c, False)
 
 controller_1 = Controller(PRIMARY)
 controller_2 = Controller(PARTNER)
@@ -46,8 +44,10 @@ controller_2 = Controller(PARTNER)
 FORWARD_SPEED_MULTIPLIER = 1
 STRAFE_SPEED_MULTIPLIER = 1
 ROTATION_SPEED_MULTIPLIER = 0.5
-
-
+CRABCRAWLALIGN = 2
+global offset
+offset = 1
+direction = FORWARD #maybe set to global?
 #================================================================= wait for stuff to configure =================================================================#
 wait(25, MSEC)
 #================================================================= wait for stuff to configure =================================================================#
@@ -55,6 +55,9 @@ wait(25, MSEC)
 
 # Main programming loop---------------------------------------------------------------------------
 def main():
+    #flywheelMotor2.spin(direction, 30)
+    #conveyorMotor.spin(direction, 30)
+    global offset
     # left joystick y axis (3)
     y = controller_1.axis3.position() * FORWARD_SPEED_MULTIPLIER
     # left joystick x axis (4)
@@ -63,35 +66,51 @@ def main():
     turn = controller_1.axis1.position() * ROTATION_SPEED_MULTIPLIER
     
     # deadband for joystick drift
-    if abs(x) < 5:
+    if abs(x) < 2:
        x = 0
-    if abs(y) < 5:
+    if abs(y) < 2:
        y = 0
-    if abs(turn) < 5:
+    if abs(turn) < 2:
        turn = 0
     
     # Convert cartesian values (x and y) into polar values (angle and magnitude)
     theta = math.atan2(y, x) 
     power = math.sqrt(float(x**2) + float(y**2))
 
+    direction = reverse()
+    intake(direction)
+    conveyor(direction)
+    shoot(direction)
 
     # move drive wheels
-    drive(power, turn, theta)
-
-
+    if(controller_1.buttonLeft.pressing()):
+        drive(50, 0, math.radians(-90), x)
+    elif(controller_1.buttonRight.pressing()):
+        drive(50, 0, math.radians(90), x)
+    elif(controller_1.buttonDown.pressing()):
+        drive(50, 0, math.radians(-180), x)
+    elif(controller_1.buttonUp.pressing()):
+        drive(50, 0, math.radians(0), x)
+    else:
+        drive(power, turn, theta, x)
 
 # Controls Robot drive-------------------------------------------------------------------------------------------
 # Forward & Turn control speed of chassiss wheels
 # Values Span -100 to 100. 
-def drive(power: float, turn: float, theta: float):
+def drive(power: float, turn: float, theta: float, strafeAxis: float):
     sin = math.sin(theta - math.pi/4)
     cos = math.cos(theta - math.pi/4)
     maxValue = max(abs(sin), abs(cos))
     
-    leftFront = power * cos/maxValue + turn
-    rightFront = power * sin/maxValue - turn
+    leftFront = (power * cos/maxValue + turn) * offset
+    rightFront = (power * sin/maxValue - turn) * offset
     leftRear = power * sin/maxValue + turn
     rightRear = power * cos/maxValue - turn
+
+    # drive left side
+    #if strafeAxis > 0 or strafeAxis < 0:
+     #   rightFront = rightFront * CRABCRAWLALIGN
+      #  leftFront = leftFront * CRABCRAWLALIGN
 
     # if one motor needs to move faster than the max, all of the motor speeds are reduced
     if ((power + abs(turn)) > 100):
@@ -104,7 +123,8 @@ def drive(power: float, turn: float, theta: float):
         leftRear *= 100
         rightRear *= 100
 
-    # drive left side
+    
+
     frontLeftMotor.spin(FORWARD, leftFront)
     backLeftMotor.spin(FORWARD, leftRear)
 
@@ -114,11 +134,8 @@ def drive(power: float, turn: float, theta: float):
 
 
     # Print motor vaules out for debugging
-    # brain.screen.clear_screen()
-    # brain.screen.set_cursor(1,1)
-    # brain.screen.print("LF: ", leftFront)
-    # brain.screen.new_line()
-    # brain.screen.print("LR: ", leftRear)
+
+    #brain.screen.print("LR: ", leftRear)
     # brain.screen.new_line()
     # brain.screen.print("RF: ", rightFront)
     # brain.screen.new_line()
@@ -130,35 +147,61 @@ def drive(power: float, turn: float, theta: float):
     # brain.screen.new_line()
     # brain.screen.print("Turn ", turn)
 
+#Shooting & Intake Mechanism----------------------------------------------------
+def intake(direction): 
+    if controller_1.buttonL1.pressing():     
+        inakeMotor.spin(direction, 30)
+    else:
+        inakeMotor.stop()
 
+# Conveyor Mechanism----------------------------------------------------
+def conveyor(direction):
+    if controller_1.buttonR1.pressing():     
+        conveyorMotor.spin(direction, 60)
+        controller_1.screen.clear_screen()
+        controller_1.screen.set_cursor(1,1)
+        controller_1.screen.print("print test ", direction)
+    else:
+        controller_1.screen.clear_screen()
+        controller_1.screen.set_cursor(1,1)
+        controller_1.screen.print("not run ", direction)
+        conveyorMotor.stop()
+
+is_flywheel_on = False
 # Shooting Mechanism----------------------------------------------------
-def shootingMechanism(): 
-    #Shooting
-    # only want to intake if button "R2" is being pressed  
-    if controller_2.buttonR2.pressing():     
-        redFlywheelMotor.spin(FORWARD, 100)  # should be spinning clockwise | speed could change through game
-        blueFlywheelMotor.spin(FORWARD, 90)  # should be spinning counterclockwise | blue speed should be constant 
+def shoot(direction):
+    global is_flywheel_on
+
+    if controller_1.buttonR2.pressing():
+        # Toggle the state when the button is pressed
+        is_flywheel_on = not is_flywheel_on
+
+    if is_flywheel_on:
+        # If the motor state is on, spin the motors
+        flywheelMotor1.spin(direction, 70)
+        flywheelMotor2.spin(direction, 70)
+    else:
+        # If the motor state is off, stop the motors
+        flywheelMotor1.stop()
+        flywheelMotor2.stop()
+
+#reverses all motors that are controlled by the object 'direction'
+#for intake and shooter
+def reverse():
+    if controller_1.buttonX.pressing():     
+        direction = REVERSE
+    else:
+        direction = FORWARD
+    return direction
     
-    elif controller_2.buttonA.pressing(): 
-        pass
-    else:
-        redFlywheelMotor.stop()
-        blueFlywheelMotor.stop()
-        pass 
+     
 
 
+    
 
-#conveyor intake Mechanism----------------------------------------------------
-def conveyorMechanism():
-    if controller_2.buttonA.pressing(): 
-        intakeWheelMotor.spin(FORWARD, 50)
-    else:
-        intakeWheelMotor.stop()
-
-
-# ---- START ACTUALLY EXECUTING CODE ---- 
-
-
+# ---- START EXECUTING CODE ---- 
 while 1:
     main()
-    wait(50, MSEC)
+    wait(1, MSEC)
+
+
